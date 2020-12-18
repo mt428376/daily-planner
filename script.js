eaad54d895384e7022dd8075b75f9ff3e3f95e81
mya todd <mya.todd428376@gmail.com>
2020-12-17 19:14:30 -0500

    starting working on the html

---
+++ script.js
@@ -0,0 +1,26 @@
+// Display the date the top of the calendar
+
+var todayDate = $("#currentDay");
+todayDate.text(moment().formant("dddd,MMMM,Do YYYY"));
+// Need time blocks for each time
+var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+var row = $("<div>");
+row.addClass("hour col-1");
+row.append(hour);
+
+var textArea = $("<textarea>");
+textArea.addClass("past description col-10");
+row.append(textArea);
+
+var button = $("<button>");
+button.addClass("saveBtn col-1");
+row.append(button);
+
+$(".container").append(row);
+
+var buttonStorage = document.getElementsByClass("textarea").value;
+    localStorage.setItem("button", buttonStorage)
+
+
+localStorage.button("textarea", JSON.stringify(textArea));
